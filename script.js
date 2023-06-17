cae75697dab59a85fdfd25fe12381a4faf757705
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-06-17 06:47:18 -0700

    Add files via upload
    For loop,while loop, do while loop.

---
+++ script.js
@@ -0,0 +1,90 @@
+
+// while loop syntax
+var i = 1;
+while (i <= 10) {
+    document.write(" I Love Bangladesh" + "<br>");
+    i++;
+//   document.write("Bangladesh" + "<br>");
+//   i++;
+}
+
+
+
+
+// sum of numbers 1+2+..+100
+var sum = 0;
+var x = 1;
+while ( x <= 100){
+    sum = sum + x + "<br>";
+    x++;
+}
+document.write(sum);
+// var sum = 0;
+// var x = 1;
+// while (x <= 100) {
+//   sum = sum + x;
+//   x++;
+// }
+
+// document.write(sum ");
+
+
+
+
+
+// do while loop syntax
+
+
+// var i = 1;
+// do {
+//     document.write("Bangladesh" + "<br>");
+//     i++;
+  
+// } while (i <= 10);
+
+// document.write("End of for loop");
+
+// document.write("End of do while loop");
+
+// sum of numbers 1+2+..+100
+// var sum = 0;
+// var x = 1;
+// do{
+//     sum = sum + x;
+//     x++;
+// }while ( x <= 100)
+// document.write(sum);
+
+
+// var sum = 0;
+// var x = 1;
+// do {
+//   sum = sum + x;
+//   x++;
+// } while (x <= 100);
+// document.write(sum);
+
+
+
+
+
+// for loop syntax
+for (var i = 1; i <= 10; i++) {
+    document.write("Bangladesh" + "<br>");
+  document.write("Bangladesh" + "<br>");
+}
+
+document.write("End of for loop");
+
+
+// sum of numbers 1+2+..+100
+// var sum = 0;
+// for (var x = 1; x <= 100; x++){
+//     sum = sum + x;
+// }
+// document.write(sum);
+var sum = 0;
+for (var x = 1; x <= 100; x++) {
+  sum = sum + x + "<br>";
+}
+document.write("The sum is " + sum );
